24482abe1e8c0a76d95402fd5a9f9dea38cbeda0
Jakob Höfner <jakob-hoefner@web.de>
2020-03-31 12:13:26 +0200

    Redacted database credentials
    DB credentials were changed and stored in GitHub secrets

--- backend/include/db_connect.php
+++ backend/include/db_connect.php
@@ -1,7 +1,7 @@
 <?php
 	$db_server = "127.0.0.1";
-	$db_name = "ni163258_1sql1";
-	$db_pw = "410a2275";
+	$db_name = "${{ secrets.database_username }}";
+	$db_pw = "${{ secrets.database_password }}";
 	$db_user = "ni163258_1sql1";
 
 	$db = new mysqli($db_server, $db_user, $db_pw, $db_name);
